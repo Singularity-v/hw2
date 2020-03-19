f4f2bed0f896755e902c5ff29b01a906b4457bfe
Crystal Wu <crystalwu.home@gmail.com>
2020-03-19 13:11:05 +0800

    Header+albumList

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import Header from "./scr/components/Header";
+import AlbumList from "./scr/components/AlbumList";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require("./imgs/2.png")} style={{width: 300,height:400,}} />
-      <Text style={styles.text}>Over the garden wall</Text> 
-      
+    <View style={styles.viewStyle}>
+       <Header />
+       <AlbumList />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#B4CEB3',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize:20,
-    color: '#546A76',
+  viewStyle: {
+    backgroundColor: '#779FA1',
 
   },
+
 });
-
 export default App;
